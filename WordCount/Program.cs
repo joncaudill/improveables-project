63002ba656c6f25741bb022ad1aa3905fdaa01ae
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-06-25 11:32:44 -0400

    finished basic working progam

---
+++ WordCount/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var filePath = args?.Length > 0 ? args[0] : "poe-fall-of-the-house-of-usher.txt";
+
+WordCount(filePath);
+
+void WordCount(string inputFilePath)
+{
+    string fileText = File.ReadAllText(inputFilePath);
+
+    int fileWordcount = fileText.Split(new char[] { ' ', '\n', '\r', '\t' },
+    StringSplitOptions.RemoveEmptyEntries).Length;
+
+    Console.WriteLine($"# of words: {fileWordcount}");
+}
